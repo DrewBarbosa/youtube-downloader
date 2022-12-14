ee235ae0af03598297c923e500824e76aeb622ed
Endrew Barbosa <endrew.ab@hotmail.com>
2022-12-14 14:53:39 -0300

    First commit

---
+++ downloader.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+
+SAVE_PATH = "C:\\Users\\famil\\Desktop"
+
+
+def download():
+    global SAVE_PATH
+
+    link = []
+
+    if link == [] or link == None:
+        return print("No video to download")
+
+    for i in link:
+        try:
+            yt = YouTube(i)
+        except:
+            print("Connection Error")
+
+        try:
+            print("Downloading video...")
+            yt.streams.filter(progressive=True, file_extension='mp4').order_by(
+                'resolution').desc().first().download(SAVE_PATH)
+        except:
+            print("Some Error!")
+    print("Task Completed!")
---
+++ main.py
@@ -0,0 +1,5 @@
+import downloader
+
+if __name__ == '__main__':
+
+    downloader.download()
